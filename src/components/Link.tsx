de693b99d40df46be4744391fafcda9475225abc
Gagandeep Singh Makhija <gagandeep.2020@gmail.com>
2025-09-11 12:56:22 +0530

    Link Component

---
+++ src/components/Link.tsx
@@ -0,0 +1,16 @@
+import NextLink from 'next/link';
+import React from 'react';
+
+export interface LinkProps {
+    href: string;
+    className?: string;
+    target?: string;
+}
+
+const Link = ({ className, ...props }: LinkProps) => (
+    <NextLink {...props} className={className} />
+);
+
+Link.displayName = 'Link';
+
+export default Link;
