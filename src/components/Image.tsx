9718a62edf971f69717f85791865594169038b21
Gagandeep Singh Makhija <gagandeep.2020@gmail.com>
2025-09-11 19:41:40 +0530

    Image component to work as wrapper in all frameworks its implementation will vary but not the interface and usage that will be same in LLM

---
+++ src/components/Image.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import NextImage, { ImageProps as NextImageProps } from 'next/image';
+
+export interface ImageProps extends Omit<NextImageProps, 'placeholder' | 'blurDataURL'> {
+    src: string;
+    alt: string;
+    width: number;
+    height: number;
+    priority?: boolean;
+    quality?: number;
+    sizes?: string;
+    fill?: boolean;
+    style?: React.CSSProperties;
+    className?: string;
+}
+
+const Image: React.FC<ImageProps> = (props) => {
+    return <NextImage {...props} />;
+};
+
+export default Image;
